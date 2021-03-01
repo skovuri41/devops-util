996a33f458a751695ab31eac4192440981e22773
shyam sunder k <shaymkovuri3@outlook.com>
2021-03-01 18:55:30 -0500

    changes arguments

--- src/clj/ucp_deploy/core.clj
+++ src/clj/ucp_deploy/core.clj
@@ -5,51 +5,68 @@
    [clj-ssh.ssh :as ssh]
    [ucp-deploy.config :refer [env]]
    [org.httpkit.client :as http]
+   [cuerdas.core :as str]
    [cli-matic.core :refer [run-cmd]]
    [mount.core :as mount])
   (:gen-class))
 
 (defn- nexus-url
-  [name version]
-  (let [endpoint (get-in env [:nexus-endpoints (keyword name)])]
-    (str endpoint version "/" (str name "-" version ".jar"))))
+  [base name version type]
+  (str base version "/" (str name "-" version "." type)))
 
 (defn download-artifact
-  [{:keys [name version]}]
-  (let [artifact-name                                      (str name "-" version ".jar")
-        endpoint                                           (nexus-url name version)
-        _                                                  (log/info endpoint)
-        {:keys [status headers body error opts] :as resp} @(http/get endpoint {:insecure? (get env :insecure)
-                                                                               :as :stream})]
+  [url insecure?]
+  (let [_                                                 (log/info url)
+        {:keys [status headers body error opts] :as resp} @(http/get url {:insecure? insecure?
+                                                                          :as        :stream})]
     (if error
-      (log/info "download artifact failure " name status error)
-      (io/copy body (io/file artifact-name)))))
+      (log/info "download artifact failure " status error)
+      (io/copy body (io/file (str/slice url (inc (str/last-index-of url "/"))))))))
 
 (defn scp-artifact
-  [{:keys [name version]}]
-  (let [artifact-name (str name "-" version ".jar")
-        hostname      (get env :ssh-hostname)
-        username      (get env :ssh-username)
-        password      (get env :ssh-password)
-        remote-path   "/tmp"
-        _             (log/info "sftp artifact: " artifact-name " to host " hostname)
-        agent (ssh/ssh-agent {})
-        session (ssh/session agent hostname {:username                 username
-                                             :password                 password
-                                             :strict-host-key-checking :no})]
-    (ssh/with-connection session
-      (let [channel (ssh/ssh-sftp session)]
-        (ssh/with-channel-connection channel
-          (ssh/sftp channel {} :cd remote-path)
-          (ssh/sftp channel {} :put artifact-name artifact-name)))
-      (ssh/ssh session {:cmd (str "chmod 755 " remote-path "/" artifact-name)})
-      (let [result (ssh/ssh session {:cmd (str "du -B 1 " remote-path "/" artifact-name)})]
-        (log/info (result :out))))))
+  [{:keys [name version type hostname username password remote-path]}]
+  (try
+    (let [artifact-name (str name "-" version "." type)
+          _             (log/info "sftp artifact: " artifact-name " to host " hostname)
+          agent         (ssh/ssh-agent {})
+          session       (ssh/session agent hostname {:username                 username
+                                                     :password                 password
+                                                     :strict-host-key-checking :no})]
+      (ssh/with-connection session
+        (let [channel (ssh/ssh-sftp session)]
+          (ssh/with-channel-connection channel
+            (ssh/sftp channel {} :cd remote-path)
+            (ssh/sftp channel {} :put artifact-name artifact-name)))
+        (ssh/ssh session {:cmd (str "chmod 755 " remote-path "/" artifact-name)})
+        (let [result (ssh/ssh session {:cmd (str "du -B 1 " remote-path "/" artifact-name)})]
+          (log/info (result :out)))))
+    (catch Exception e (str "caught exception: " (.getMessage e)))))
 
 (comment
-  (nexus-url "commons-io" "2.8.0")
-  (download-artifact {:name "commons-io" :version "2.8.0"})
-  (scp-artifact {:name "commons-io" :version "2.8.0"}))
+
+  (let [{:keys [base name version type] :as params} (get env :commons-io)
+        url       (nexus-url base name version type)
+        insecure? (get env :insecure)]
+    (download-artifact url insecure?))
+
+  (let [scp-args {:hostname    (get env :ssh-hostname)
+                  :username    (get env :ssh-username)
+                  :password    (get env :ssh-password)
+                  :remote-path (get env :remote-path)}
+        artifact-args (get env :commons-io)]
+    (scp-artifact (merge scp-args artifact-args)))
+
+  (let [{:keys [base name version type] :as artifact-args} (get env :commons-io)
+        url                                                (nexus-url base name version type)
+        insecure?                                          (get env :insecure)
+        scp-args                                           {:hostname    (get env :ssh-hostname)
+                                                            :username    (get env :ssh-username)
+                                                            :password    (get env :ssh-password)
+                                                            :remote-path (get env :remote-path)}]
+    (download-artifact url insecure?)
+    (scp-artifact (merge scp-args artifact-args)))
+
+  )
 
 (defn deploy
   [artifact])
@@ -57,6 +74,13 @@
 (defn -main [& args]
   (mount/start)
   (log/info "Started...")
-  (download-artifact {:name "commons-io" :version "2.8.0"})
-  (scp-artifact {:name "commons-io" :version "2.8.0"})
+  (let [{:keys [base name version type] :as artifact-args} (get env :commons-io)
+        url                                                (nexus-url base name version type)
+        insecure?                                          (get env :insecure)
+        scp-args                                           {:hostname    (get env :ssh-hostname)
+                                                            :username    (get env :ssh-username)
+                                                            :password    (get env :ssh-password)
+                                                            :remote-path (get env :remote-path)}]
+    (download-artifact url insecure?)
+    (scp-artifact (merge scp-args artifact-args)))
   (log/info "Completed..."))
